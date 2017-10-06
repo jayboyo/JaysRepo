9ffeefda9b83d669daf4acd93548d644e528ad65
jayboyo <jaymun123@gmail.com>
2017-10-06 17:12:54 +0100

    Networking2

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,15 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class PlayerMovement : MonoBehaviour {
+public class PlayerMovement : NetworkBehaviour {
 
 
     public float speed = 10.0f;
     public Rigidbody rb;
 
     Vector3 movement;
-    Quaternion newRotation;
+
 
     private void Awake()
     {
@@ -23,8 +24,21 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        transform.Rotate(0, x, 0);
+        transform.Translate(0, 0, z);
+            
+
+    }
 
     private void FixedUpdate()
     {
@@ -34,8 +48,8 @@ public class PlayerMovement : MonoBehaviour {
         float mh = Input.GetAxis("Mouse Y");
         float mv = Input.GetAxis("Mouse X");
 
-        Move(h, v);
-        Rotate(mh, mv);
+        //Move(h, v);
+        //Rotate(mh, mv);
 
     }
 
@@ -49,8 +63,6 @@ public class PlayerMovement : MonoBehaviour {
         movement = new Vector3(0,0,0);
 
         movement = (transform.forward * v * speed * Time.deltaTime) + (transform.right * h * speed * Time.deltaTime);
-
-
 
         rb.MovePosition(transform.position + movement);
 
