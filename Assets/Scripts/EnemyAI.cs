16a63ef87c6ecfacf3db72b117042325b9cf0fe8
jayboyo <jaymun123@gmail.com>
2017-10-08 16:39:55 +0100

    WithAlienModel

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour {
+
+    UnityEngine.AI.NavMeshAgent nav;
+    Vector3 moveLocation;
+
+    private void Awake()
+    {
+        nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+
+        moveLocation = new Vector3(Random.Range(-100.0f, 100.0f), 0, Random.Range(-100.0f, 100.0f));    // Sets random move location
+        nav.SetDestination(moveLocation);
+    }
+    	
+	void Update () {
+
+        if (nav.remainingDistance < 10.0f) {    //  Target is close enough to move marker so set new one
+
+            moveLocation = new Vector3(Random.Range(-100.0f, 100.0f), 0, Random.Range(-100.0f, 100.0f));
+            nav.SetDestination(moveLocation);
+        }
+        
+
+    }
+}
---
+++ Assets/Scripts/LobbyPlayerScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyPlayerScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        GameObject[] playerSpawns = GameObject.FindGameObjectsWithTag("lobbySpawn");
+
+        foreach (GameObject x in playerSpawns)
+        {
+            if (!x.GetComponent<LobbySpawnPoint>().inUse)
+            {
+                transform.position = x.transform.position;
+                x.GetComponent<LobbySpawnPoint>().inUse = true;
+                break;
+            }
+
+        }
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
